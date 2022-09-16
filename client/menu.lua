d3738c293248749e56b1525209cba6c995c1e422
swkeep <49286776+swkeep@users.noreply.github.com>
2022-09-16 19:57:25 +0430

    sorted menu

--- client/menu.lua
+++ client/menu.lua
@@ -131,26 +131,6 @@ local menu = {
     }
 }
 
--- function Draw3DText(x, y, z, text)
---     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
---     local px, py, pz = table.unpack(GetGameplayCamCoords())
-
---     if onScreen then
---         SetTextScale(0.35, 0.35)
---         SetTextFont(4)
---         SetTextProportional(1)
---         SetTextColour(255, 255, 255, 215)
---         SetTextDropShadow(0, 0, 0, 55)
---         SetTextEdge(0, 0, 0, 150)
---         SetTextDropShadow()
---         SetTextOutline()
---         SetTextEntry("STRING")
---         SetTextCentre(1)
---         AddTextComponentString(text)
---         DrawText(_x, _y)
---     end
--- end
-
 local coo = {
     [1] = {
         offset = vector4(-1.5, 0.0, 0.0, -90.0),
@@ -398,7 +378,7 @@ AddEventHandler('keep-companion:client:action_menu', function()
         }
     }
 
-    for key, value in pairs(menu) do
+    for key, value in ipairs(menu) do
         if value.show then
             if not value.show(ActivePed.read()) then
                 goto here
